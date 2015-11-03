e2aae088f14b2a9e0b89190a865cae924f2c82f3
AndrewTBurks <andrewtburks@gmail.com>
2015-11-02 19:03:33 -0600

    Abstract Node parent class for Node2, Node3, Node4
    This Node class will be the parent of Node2, Node3, and Node4 for the
    2-3-4 tree. There are 2 abstract methods that are required to be
    overridden by the subclass. Also some accessor methods are included in
    this class.

---
+++ Node.java
@@ -0,0 +1,48 @@
+import java.awt.Graphics;
+
+public abstract class Node
+{
+    private int[] keys;         // the keys (1, 2, or 3) of the node
+    private Node[] children;    // the children (2, 3, or 4) of the node
+    private Node parent;        // the parent of this node (null if root)
+
+    private ScaledPoint coord;
+
+    public abstract void drawNode(Graphics g);
+
+    public abstract Node findPath(int n);
+
+    public boolean isLeaf()
+    // POST: FCTVAL == TRUE if this Node has no children, FALSE otherwise
+    {
+        // iterate through children to see if any aren't null
+        // there should really be only 2 cases, all children are null or no children are null
+        
+        if(children[0] == null) // if the child exists
+        {
+            return true; 
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public int[] getKeys()
+    // POST: FCTVAL == an int[] keys of this Node
+    {
+        return keys;
+    }
+    
+    public Node[] getChildren()
+    // POST: FCTVAL == a Node[] of children of this Node
+    {
+        return children;
+    }
+    
+    public Node getParent()
+    // POST: FCTVAL == the parent of this Node
+    {
+        return parent;
+    }
+}

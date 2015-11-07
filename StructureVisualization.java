0b2266f48c8e705328eefe11580f77749c0f5ca6
tomar3 <talab.omar@gmail.com>
2015-11-07 00:57:23 -0600

    accidentally deleted an import statement
    popped it back in

--- StructureVisualization.java
+++ StructureVisualization.java
@@ -8,7 +8,7 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.*;
-
+import javax.swing.JApplet;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Color; 
